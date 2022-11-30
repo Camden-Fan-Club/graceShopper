0714b218cc5e8b3ab95b8372ff17db2e0465137a
bada <bada_0524@gmail.com>
2022-11-30 10:42:28 -0800

    testing error

--- client/src/hooks/useUser.jsx
+++ client/src/hooks/useUser.jsx
@@ -1,11 +1,18 @@
 import { useStoreState, useStoreActions } from "easy-peasy";
 
 export default function useUsers() {
-  const registerUser = useStoreActions(
-    (action) => useStoreActions.users.registerUser
+  const createUser = useStoreActions(
+    (actions) => useStoreActions.users.createUser
   );
 
   const users = useStoreState((state) => state.users.data);
-  const loginUser = useStoreActions((action) => useStoreActions.users.data);
+  const loginUser = useStoreActions((actions) => useStoreActions.users.data);
   const fetchUsers = useStoreActions((actions) => actions.users.fetchUsers);
+
+  return {
+    createUser,
+    users,
+    loginUser,
+    fetchUsers,
+  };
 }
