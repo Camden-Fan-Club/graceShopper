0cbfe7911342431dd67f0220a82940a68a5afb6a
tanner <tannerlessi@gmail.com>
2022-12-02 10:45:15 -0800

    cart store/hooks

---
+++ client/src/store/models/cart.js
@@ -0,0 +1,13 @@
+import { action, thunk } from "easy-peasy";
+import axios from "axios";
+
+export const cart = {
+  data: [],
+  setCart: action((state, payload) => {
+    state.data = payload;
+  }),
+};
+fetchCart: thunk(async (actions, payload) => {
+  const { data } = await axios.get("/routes/users/:userId/cart");
+  actions.setCart(data);
+});
