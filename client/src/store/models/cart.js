f3ee4c64cf5d0c835713be7a7b6658f76d932d60
tanner <tannerlessi@gmail.com>
2022-12-05 19:49:05 -0800

    error

--- client/src/store/models/cart.js
+++ client/src/store/models/cart.js
@@ -8,10 +8,20 @@ export const cart = {
   }),
 
   fetchCart: thunk(async (actions, payload) => {
-    const { data } = await axios.get(`/routes/users/${payload}/cart`);
+    console.log("PAYLOAD", payload);
+    const { data } = await axios.get(`/routes/users/${payload.userId}/cart`);
     actions.setCart(data);
   }),
-
+  addItem: action((state, payload) => {
+    state.data.push(payload);
+  }),
+  //POST ITEM TO CART
+  addItemToCart: thunk(async (actions, payload) => {
+    const { data } = await axios.post(
+      `/routes/order_items/${payload.itemId}/${payload.orderId}`
+    );
+    actions.addItem(data);
+  }),
   //UPDATE
 
   //DELETE
