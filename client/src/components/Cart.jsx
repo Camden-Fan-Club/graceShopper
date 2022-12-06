634fed106bb4af008a0cff5d8a3b0605bb91b6cc
Carson Craft <ccraft4444@gmail.com>
2022-12-06 12:43:59 -0800

    can update cart qty

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,29 +2,55 @@ import { useEffect } from "react";
 import React from "react";
 import useAuth from "../hooks/useAuth";
 import useCart from "../hooks/useCart";
+import { useNavigate } from "react-router-dom";
 
 export default function Cart() {
-  const { cart, fetchCart } = useCart();
-  const { selectedUser } = useAuth();
+  const { cart, fetchCart, updateQuantity } = useCart();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("selecteduser in cart", selectedUser);
-    const userId = selectedUser.id;
-    fetchCart(userId);
+    fetchCart();
   }, []);
-  console.log(cart);
+  console.log("cart after use effect", cart);
 
   return (
     <div>
       {" "}
       My Cart
-      {cart.order_items.map((items) => {
-        const item = items.items;
+      {cart?.order_items?.map((orderItem) => {
+        const item = orderItem.items;
         return (
           <div>
             <img className="h-40 mt-0" src={item.imageUrl} />
             <h3>{item.name}</h3>
-            <h4>{items.quantity}</h4>
+            <h4>Price: ${item.price}</h4>
+            <h4>Quantity:{orderItem.quantity}</h4>
+
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                console.log("submitting form");
+                console.log(item.id);
+                const quantity = e.target[0].value;
+                console.log("qty", quantity);
+
+                await updateQuantity({
+                  itemId: item.id,
+                  orderId: cart.id,
+                  quantity: +quantity,
+                });
+              }}
+            >
+              <select>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </select>
+              <button type="submit">Update Quantity</button>
+            </form>
           </div>
         );
       })}
